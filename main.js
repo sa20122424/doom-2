86fd35cfefd9599bb055eb439522d61c160b96d7
sa20122424 <saloxiddin201224@gmail.com>
2024-08-17 12:21:06 +0500

    salom

---
+++ main.js
@@ -0,0 +1,29 @@
+// let ism = document.querySelector(".ism")
+
+// ism.style.textAlign = "center"
+// ism.innerHTML = "<h1>Abducarimov <mark>Salohiddin</mark></h1> "
+// ism.style.backgroundColor = "red"
+// ism.style.border = 'solid 5px '
+// ism.style.borderRadius = ' 95px '
+
+
+// let mars1 = document.querySelector(".mars")
+// mars1.style.textAlign = "end"
+// mars1.style.backgroundColor = "yellow"
+// mars1.style.border = 'solid 10px '
+// mars1.style.borderRadius = ' 95px '
+// let adres1 = document.querySelector(".adres")
+
+// adres1.style.textAlign = "center"
+// adres1.style.backgroundColor = "green"
+// adres1.style.border = 'solid 10px '
+// adres1.style.borderRadius = ' 95px ' 
+
+let bu = document.querySelector(".p")
+
+let ism3 = prompt('ismingiz kiriting')
+// let ism4 = prompt('rang kiriting')
+bu.style. textAlign = "center"
+bu.style. marginTop = "300px"
+bu.innerHTML = `<h1>${ism3}</h1>`
+
